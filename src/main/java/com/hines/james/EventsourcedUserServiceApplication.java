e8071b71466947afd65141898b4e7bf275ef76ed
James Hines <jameshines10@gmail.com>
2017-09-11 07:26:34 -0400

    Initial commit

---
+++ src/main/java/com/hines/james/EventsourcedUserServiceApplication.java
@@ -0,0 +1,12 @@
+package com.hines.james;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsourcedUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsourcedUserServiceApplication.class, args);
+	}
+}
